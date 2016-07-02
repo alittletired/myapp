42059898fa0fe20f38a467c86f504bf307efd569
unknown <zhul03@mysoft.com.cn>
2016-07-02 15:21:09 +0800

    haha

--- src/js/MyApp.js
+++ src/js/MyApp.js
@@ -30,6 +30,31 @@ const RouterWithRedux = connect()(Router);
 //        return defaultReducer(state, action);
 //    }
 //};
+
+class NavigationDrawer extends React.Component {
+    render() {
+        const state = this.props.navigationState;
+        const children = state.children;
+        return (
+            <Drawer
+                ref="navigation"
+                type="displace"
+                onOpen={() => Actions.refresh({ key: state.key, open: true }) }
+                onClose={() => Actions.refresh({ key: state.key, open: false }) }
+                content={<TabView />}
+                tapToClose
+                openDrawerOffset={0.2}
+                panCloseMask={0.2}
+                negotiatePan
+                tweenHandler={(ratio) => ({
+                    main: { opacity: Math.max(0.54, 1 - ratio) },
+                }) }
+                >
+                <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
+            </Drawer>
+        );
+    }
+}
 class MyApp extends React.Component {
 
     onPress() {
@@ -44,18 +69,18 @@ class MyApp extends React.Component {
         }
         return (<RouterWithRedux  rightButtonTextStyle={{ color: 'white' }} navigationBarStyle={{ backgroundColor: '#199BFC' }} tabBarStyle={{ borderColor: "#666666", borderTopWidth: 1, backgroundColor:'white'}}  sceneStyle={{}} titleStyle={{  color: 'white',fontSize:20 }}>
             <Scene key="modal" component={Modal} >
-                <Scene key="root" tabs={true} >
+                <Scene key="root"  hideNavBar hideTabBar     >
 
                     <Scene key="Problem" component={Problem} title="问题"  icon={TabIcon} initial={true}
-                        rightTitle="新增" onRight={() => { alert(1);  this.onPress.bind(this) }  } >
+                        rightTitle="新增" onRight={() => { alert(1); this.onPress.bind(this) } }  tabs={true}>
 
-
+                        <Scene key="ProblemDetials"   component={ProblemDetials} title="问题详情"
+                            onLeft={() => { Actions.pop() } } leftTitle="问题" leftBotonImage={}  hideNavBar={false}
+                            />
                       
                     </Scene>
                     <Scene key="Setting" component={Setting} title="设置"  icon={TabIcon}/>
-                    <Scene key="ProblemDetials"   component={ProblemDetials} title="问题详情"
-                        tabs={false}
-                        />
+                    
                     <Scene key="addDetailScreen"  hideTabBar={true} component={addDetailScreen} title="添加问题"
                         tabs={false}
                         />
